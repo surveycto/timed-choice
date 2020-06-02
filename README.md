9ab363c9e87832d7f83bea7cbe1fdf07e588f87a
Max S. Haberman <max@surveycto.com>
2020-06-01 20:03:30 -0400

    Initial adding of files

---
+++ README.md
@@ -0,0 +1,102 @@
+# Timed field list - choice lists
+
+![Quick appearance](extras/preview-images/quick.png)  
+*select_one* with "quick" *appearance*
+
+| numeric | select_one | select_multiple |
+| --- | --- | --- |
+| <img src="extras/preview-images/numeric.png" alt="numeric" title="numeric" width="100px"/> | <img src="extras/preview-images/select_one.png" alt="select_one" title="select_one" width="100px"/> | <img src="extras/preview-images/select_multiple.png" alt="select_multiple" title="select_multiple" width="100px"/> |
+
+## Description
+
+*Requires at least Android 7 or iOS 13 to work in SurveyCTO Collect mobile.*
+
+Use this field when you would like to time multiple *select_one* and/or *select_multiple* fields within the same field list. within the same [field list](https://docs.surveycto.com/02-designing-forms/04-sample-forms/05.field-lists.html).
+
+If a user attempts to return to a field with this field plug-in that has already been completed, the field will auto-advance (unless parameter 4 is equal to `1` and there was time left, see **Parameters** below).
+
+[![Download now](extras/download-button.png)](https://github.com/surveycto/timed-advance/raw/master/timedadvance.fieldplugin.zip)
+
+## Default SurveyCTO feature support
+
+| Feature / Property | Support |
+| --- | --- |
+| Supported field type(s) | `select_one`, `select_multiple`|
+| Default values | Yes |
+| Custom constraint message | Yes |
+| Custom required message | Yes |
+| Read only | Yes |
+| media:image | Yes |
+| media:audio | Yes |
+| media:video | Yes |
+| `label` appearance | Not yet |
+| `list-nolabel` appearance | Not yet |
+| `quick` appearance | Yes (`select_one` only) |
+| `minimal` appearance | Yes (`select_one` only) |
+| `compact` appearance | No |
+| `compact-#` appearance | No |
+| `quickcompact` appearance | No |
+| `quickcompact-#` appearance | No |
+| `likert` appearance | Yes (`select_one` only) |
+| `likert-min` appearance | Yes* (`select_one` only) |
+| `likert-mid` appearance | No |
+
+## How to use
+
+**To use this field plug-in as-is**, just download the [timedadvance.fieldplugin.zip](timedadvance.fieldplugin.zip) file from this repo, and attach it to your form.
+
+To create your own field plug-in using this as a template, follow these steps:
+
+1. Fork this repo
+1. Make changes to the files in the `source` directory.
+
+    * **Note:** be sure to update the `manifest.json` file as well.
+
+1. Zip the updated contents of the `source` directory.
+1. Rename the .zip file to *yourpluginname*.fieldplugin.zip (replace *yourpluginname* with the name you want to use for your field plug-in).
+1. You may then attach your new .fieldplugin.zip file to your form as normal.
+
+**Important:** You need to include a choice with the value of `-99`. This choice will be hidden by the field plug-in, but it will be selected if the time runs out without a choice selected.
+
+## Parameters
+There are five parameters, but all of them are optional:
+|**Number**|**Name**|**Description**|**Default**|
+|---|---|---|
+|`top`|Whether this is the top field of the field list. This shows where the timer should go. The first field should have a value of `1` for this parameter, and the others should not use this parameter at all.|`0`|
+|`duration`|Time in seconds before the field auto-advances. No matter what unit is used for parameter 2, you should always enter the duration in seconds.|`10`|
+|`unit`|Only needed for the first field in the field list. Unit to be displayed for the time remaining. The time will be shown as the correct converted version. For example, if the start time is 15 seconds, and the unit is `'ms'` for milliseconds, the time will be displayed at the start as `15000`.|`'s'`|
+|`pass`|The value the field will be given if time runs out before an answer is given.|`-99`|
+|`continue`|Whether a respondent can return to a field and continue with the time they have left. For example, if there was 5 seconds remaining when they swiped forward, they can return to that field and work with that remaining 5 seconds. To allow this, give this parameter a value of `1`.|`0`|
+
+For parameter 2, you can use the following display units:
+
+|**Abbr.**|**Full name**|**Unit in 1 second**|
+|:---|:---|:---|
+|`s`|seconds|1
+|`ds`|deciseconds|10
+|`cs`|centiseconds|100
+|`ms`|milliseconds|1000
+
+For example, if you would like the field to move forward after 20 seconds, you can use this *appearance*:
+
+    custom-timedadvance(duration=20)
+
+If you would like the time to be displayed in milliseconds, you can use this *appearance*:
+
+    custom-timedadvance(duration=20, unit='ms')
+
+If the field is of type *select_one*, you would like it to have the `quick` appearance, and the field should last 15 seconds, you can use this *appearance*:
+
+    quick custom-timedadvance(duration=15)
+
+If you would like the respondent to have 15 seconds to complete the field, but they can return to it later to change their answer with their remaining time, you can use this *appearance*:
+
+    quick custom-timedadvance(duration=15, unit='s', pass=-99, continue=1)
+
+## More resources
+
+* **Test form**  
+You can find a form definition in this repo here: [extras/test-form](extras/test-form).
+
+* **Developer documentation**  
+More instructions for developing and using field plug-ins can be found here: [https://github.com/surveycto/Field-plug-in-resources](https://github.com/surveycto/Field-plug-in-resources)
---
+++ source/script.js
@@ -0,0 +1,183 @@
+/* global fieldProperties, setAnswer, goToNextField */
+
+const choices = fieldProperties.CHOICES
+const appearance = fieldProperties.APPEARANCE
+const fieldType = fieldProperties.FIELDTYPE
+const numChoices = choices.length
+
+var radioButtonsContainer = document.getElementById('radio-buttons-container') // default radio buttons
+var selectDropDownContainer = document.getElementById('select-dropdown-container') // minimal appearance
+var likertContainer = document.getElementById('likert-container') // likert
+const choiceContainers = document.querySelectorAll('.choice-container') // go through all the available choices
+
+var startTime // This will get an actual value when the timer starts in startStopTimer()
+var timeStart = 10000 // Default values may be overwritten depending on the number of paramaters given,
+var unit = 's' // Default, may be changed
+var round = 1000 // Default, may be changed
+var missed = -99 // Default, may be changed
+var timeLeft // Starts this way for the display.
+var timePassed = 0 // Time passed so far
+
+
+
+if (fieldType === 'select_multiple') { // Changes input type
+  for (let c = 0; c < numChoices; c++) {
+    const choice = choices[c]
+    const container = choiceContainers[c]
+    const box = container.querySelector('INPUT')
+    box.type = 'checkbox'
+    if (choice.CHOICE_SELECTED) {
+      box.checked = true
+    }
+  }
+}
+
+// Prepare the current webview, making adjustments for any appearance options
+if ((appearance.includes('minimal') === true) && (fieldType === 'select_one')) { // minimal appearance
+  radioButtonsContainer.parentElement.removeChild(radioButtonsContainer) // remove the default radio buttons
+  likertContainer.parentElement.removeChild(likertContainer) // remove the likert container
+  selectDropDownContainer.style.display = 'block' // show the select dropdown
+} else if ((appearance.includes('likert') === true) && (fieldType === 'select_one')) { // likert appearance
+  radioButtonsContainer.parentElement.removeChild(radioButtonsContainer) // remove the default radio buttons
+  selectDropDownContainer.parentElement.removeChild(selectDropDownContainer) // remove the select dropdown contrainer
+  likertContainer.style.display = 'flex' // show the likert container
+  // likert-min appearance
+  if (appearance.includes('likert-min') === true) {
+    var likertChoices = document.getElementsByClassName('likert-choice-container')
+    for (var i = 1; i < likertChoices.length - 1; i++) {
+      likertChoices[i].querySelector('.likert-choice-label').style.display = 'none' // hide all choice labels except the first and last
+    }
+    likertChoices[0].querySelector('.likert-choice-label').classList.add('likert-min-choice-label-first') // apply a special class to the first choice label
+    likertChoices[likertChoices.length - 1].querySelector('.likert-choice-label').classList.add('likert-min-choice-label-last') // apply a special class to the last choice label
+  }
+} else { // all other appearances
+  if (fieldProperties.LANGUAGE !== null && isRTL(fieldProperties.LANGUAGE)) {
+    radioButtonsContainer.dir = 'rtl'
+  }
+
+  selectDropDownContainer.parentElement.removeChild(selectDropDownContainer) // remove the select dropdown container
+  likertContainer.parentElement.removeChild(likertContainer) // remove the likert container
+  // quick appearance
+  if ((appearance.includes('quick') === true) && (fieldType === 'select_one')) {
+    for (var i = 0; i < choiceContainers.length; i++) {
+      choiceContainers[i].classList.add('appearance-quick') // add the 'appearance-quick' class
+      choiceContainers[i].getElementsByClassName('choice-label-text')[0].insertAdjacentHTML('beforeend', '<svg class="quick-appearance-icon"><use xlink:href="#quick-appearance-icon" /></svg>') // insert the 'quick' icon
+    }
+  }
+}
+
+// minimal appearance
+if ((appearance.includes('minimal') === true) && (fieldType === 'select_one')) {
+  selectDropDownContainer.onchange = change // when the select dropdown is changed, call the change() function (which will update the current value)
+} else if ((appearance.includes('likert') === true) && (fieldType === 'select_one')) { // likert appearance
+  var likertButtons = document.querySelectorAll('div[name="opt"]')
+  for (var i = 0; i < likertButtons.length; i++) {
+    likertButtons[i].onclick = function () {
+      // clear previously selected option (if any)
+      var selectedOption = document.querySelector('.likert-input-button.selected')
+      if (selectedOption) {
+        selectedOption.classList.remove('selected')
+      }
+      this.classList.add('selected') // mark clicked option as selected
+      change.apply({ value: this.getAttribute('data-value') }) // call the change() function and tell it which value was selected
+    }
+  }
+} else { // all other appearances
+  var buttons = document.querySelectorAll('input[name="opt"]')
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onchange = function () {
+      // remove 'selected' class from a previously selected option (if any)
+      var selectedOption = document.querySelector('.choice-container.selected')
+      if ((selectedOption) && (fieldType === 'select_one')) {
+        selectedOption.classList.remove('selected')
+      }
+      this.parentElement.classList.add('selected') // add 'selected' class to the new selected option
+      change.apply(this) // call the change() function and tell it which value was selected
+    }
+  }
+}
+
+
+if (!error) {
+  setInterval(timer, 1)
+}
+
+// FUNCTIONS
+function clearAnswer () {
+  // minimal appearance
+  if (appearance.includes('minimal') === true) {
+    selectDropDownContainer.value = ''
+  } else if (appearance.includes('likert') === true) { // likert appearance
+    var selectedOption = document.querySelector('.likert-input-button.selected')
+    if (selectedOption) {
+      selectedOption.classList.remove('selected')
+    }
+  } else { // all other appearances
+    var selectedOption = document.querySelector('input[name="opt"]:checked')
+    if (selectedOption) {
+      selectedOption.checked = false
+      selectedOption.parentElement.classList.remove('selected')
+    }
+  }
+}
+
+// Save the user's response (update the current answer)
+function change () {
+  if (fieldType === 'select_one') {
+    setAnswer(this.value)
+    // If the appearance is 'quick', then also progress to the next field
+    if (appearance.includes('quick') === true) {
+      goToNextField()
+    }
+  } else {
+    const selected = []
+    for (let c = 0; c < numChoices; c++) {
+      if (choiceContainers[c].querySelector('INPUT').checked === true) {
+        selected.push(choices[c].CHOICE_VALUE)
+      }
+    }
+    setAnswer(selected.join(' '))
+  }
+}
+
+// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < and > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
+function unEntity (str) {
+  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+}
+if (fieldProperties.LABEL) {
+  document.querySelector('.label').innerHTML = unEntity(fieldProperties.LABEL)
+}
+if (fieldProperties.HINT) {
+  document.querySelector('.hint').innerHTML = unEntity(fieldProperties.HINT)
+}
+
+// Detect right-to-left languages
+function isRTL (s) {
+  var ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'
+  var rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC'
+  var rtlDirCheck = new RegExp('^[^' + ltrChars + ']*[' + rtlChars + ']')
+
+  return rtlDirCheck.test(s)
+}
+
+function timer () {
+  if (!complete) {
+    timePassed = Date.now() - startTime
+    timeLeft = timeStart - timePassed
+  }
+
+  if (timeLeft < 0) { // Timer ended
+    complete = true
+    timeLeft = 0
+    timerDisp.innerHTML = String(Math.ceil(timeLeft / round))
+
+    if ((currentAnswer == null) || (Array.isArray(currentAnswer) && (currentAnswer.length === 0))) {
+      setAnswer(missed)
+    }
+    setMetaData(0)
+    goToNextField()
+  }
+  setMetaData(timeLeft)
+
+  timerDisp.innerHTML = String(Math.ceil(timeLeft / round))
+}
